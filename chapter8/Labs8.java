b53b42dc786ab810ac9af8a00cabec0c17467df9
pmdavis1 <31441846+pmdavis1@users.noreply.github.com>
2018-04-06 09:18:11 -0500

    recurse

---
+++ chapter8/Labs8.java
@@ -0,0 +1,90 @@
+
+
+/**
+ * Write a description of class Labs8 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Labs8
+{
+   public static void main(String[] args)
+   {
+    
+      // nums(100);
+      //nums2(100);
+      //revString("waleed");
+//       int n = 73;
+//       int x = prime(n,2);
+//        if(x == 1)
+//         {
+//             System.out.println(n+" is prime number");
+//         }
+//         else
+//         {
+//             System.out.println(n+" is not prime number");
+//         }
+   System.out.print(fibb(12));
+   }
+   
+   public static void nums(int n)
+   {
+       if (n==0)
+       System.out.println("0");
+       else
+       {
+           System.out.println(n);
+           nums(n-1);	
+       }
+   }
+ 
+   public static void nums2(int n)
+    {
+        if (n==0)
+        System.out.println("0");
+        else
+        {
+            nums(n-1);	
+            System.out.println(n);
+        }
+    }
+   
+   public static void revString(String b)
+   {
+      if (b.length()-1 >= 0)
+      {
+      System.out.print(b.charAt(b.length()-1));
+      revString(b.substring(0,b.length() -1));
+      }
+    
+    }
+   
+   static int prime(int y,int i)
+    {
+         if(i < y)
+        {
+            if(y % i != 0) 
+            {
+                return(prime(y, ++i));
+            } 
+            else
+            {
+                return 0; 
+            }
+        }
+        return 1;
+    }
+    
+   static int fibb(int n)
+   {
+       if(n == 0)
+        return 0;
+       else if(n == 1)
+        return 1;
+       else
+       {
+        return fibb(n - 1) + fibb(n - 2);
+       }
+        
+   }
+}
